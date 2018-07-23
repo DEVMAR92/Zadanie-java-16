1ebbc001481dd180afd7eb371713fdbaf7ad2ec6
Marcin <Majsyn@gmail.com>
2018-07-23 12:30:13 +0200

    v 1.1 poprawione błędy.

--- src/Test.java
+++ src/Test.java
@@ -5,19 +5,20 @@ public class Test {
     public static void main(String[] args) {
         Random random = new Random();
         int[] tab = new int[10];
-        for (int i = 0; i <tab.length ; i++) {
+        for (int i = 0; i < tab.length; i++) {
             tab[i] = random.nextInt(100);
         }
         System.out.println(Arrays.toString(tab));
         int turn;
 
-        for (int i : tab) {
-            System.out.print(i + " ");
-        }
-
-        for (int i = 0; i <tab.length ; i++) {
-            turn = tab.length - i;
-            System.out.print(tab[turn-1] + " ");
+        for (int i = 0; i < tab.length; i++) {
+            System.out.print(tab[i] + " ");
+            if (i == tab.length - 1) {
+                for (int j = 0; j < tab.length; j++) {
+                    turn = tab.length - j;
+                    System.out.print(tab[turn - 1] + " ");
+                }
+            }
         }
     }
 }
