125f12ee6bdc68872b0b3c49b9e77454899d8bb6
Marcin <Majsyn@gmail.com>
2018-08-02 12:55:23 +0200

    v 1.2 jedna pętla

--- src/Test.java
+++ src/Test.java
@@ -1,24 +1,21 @@
-import java.util.Arrays;
 import java.util.Random;
 
 public class Test {
     public static void main(String[] args) {
         Random random = new Random();
         int[] tab = new int[10];
-        for (int i = 0; i < tab.length; i++) {
-            tab[i] = random.nextInt(100);
-        }
-        System.out.println(Arrays.toString(tab));
-        int turn;
 
-        for (int i = 0; i < tab.length; i++) {
-            System.out.print(tab[i] + " ");
-            if (i == tab.length - 1) {
-                for (int j = 0; j < tab.length; j++) {
-                    turn = tab.length - j;
-                    System.out.print(tab[turn - 1] + " ");
-                }
+        int turn = 10;
+
+        for (int i = 0; i < tab.length * 2; i++) {
+            if (i < tab.length){
+                tab[i] = random.nextInt(1000);
+                System.out.print(tab[i] + ", ");
+            }else {
+                System.out.print(tab[turn-1] + ", ");
+                turn--;
             }
+
         }
     }
 }
